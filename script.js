e5ff9fbbe2adff4dfe7c4162d0a958d49c6a8406
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 11:19:08 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //your JS code here. If required.
+ 
 const line = document.getElementById('line');
-let angle = 0;
-
-function rotateLine() {
-    angle = (angle + 2) % 360;
-    line.style.transform = `rotate(${angle}deg)`;
-    setTimeout(rotateLine, 20);
-}
-
-rotateLine();
+let rotation = 0;
+ 
+setInterval(() => {
+  rotation += 5;
+  line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+}, 50);
